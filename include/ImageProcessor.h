128885ad671787394b49d248760a539d557dd9de
Gluckasz <lukas.gluck.04@gmail.com>
2024-10-06 20:35:06 +0200

    Added ImageProcessor.h interface.

---
+++ include/ImageProcessor.h
@@ -0,0 +1,20 @@
+//
+// Created by gluckasz on 10/6/24.
+//
+
+#ifndef IMAGEPROCESSOR_H
+#define IMAGEPROCESSOR_H
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+class ImageProcessor {
+public:
+    virtual void modifyBrightness() = 0;
+protected:
+    Mat image;
+    bool isBrightnessModified;
+    int brightnessModVal;
+};
+
+#endif //IMAGEPROCESSOR_H
