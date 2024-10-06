e86fc53d2805ba8e64477178d07617b1e3793859
Gluckasz <lukas.gluck.04@gmail.com>
2024-10-06 19:48:14 +0200

    Added GrayscaleImageProcessor class.

---
+++ include/GrayscaleImageProcessor.h
@@ -0,0 +1,16 @@
+//
+// Created by gluckasz on 10/6/24.
+//
+
+#ifndef GRAYSCALEIMAGEPROCESSOR_H
+#define GRAYSCALEIMAGEPROCESSOR_H
+
+
+
+class GrayscaleImageProcessor {
+
+};
+
+
+
+#endif //GRAYSCALEIMAGEPROCESSOR_H
---
+++ src/GrayscaleImageProcessor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gluckasz on 10/6/24.
+//
+
+#include "../include/GrayscaleImageProcessor.h"
