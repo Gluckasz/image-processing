df64fd7a54b670f346c0763b3fcfca81b23413e4
Gluckasz <lukas.gluck.04@gmail.com>
2024-10-07 21:10:53 +0200

    Added to main.cpp linear contrast modification, negative and a functionality to stop program if --help encountered.

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,24 @@
 using namespace cv;
 using namespace std;
 
+// TODO Variant N4
 // Constants
 constexpr int INPUT_IMAGE_POS = 1;
 
 // Print available functions in command-line
 void printCommands() {
     cout << "Available commands:\n"
-    << "--help - print list of all the available commands with detailed description of their arguments.\n"
+    << "--help - print list of all the available commands with detailed description of their arguments.\n\n"
     << "--output [-outputFileName=value] - provide output file name.\n"
-    << "\t -outputFileName - output file name (default is output.bmp).\n"
-    << "--grayscale - read image in grayscale.\n"
+    << "\t -outputFileName - output file name (default is output.bmp).\n\n"
+    << "--grayscale - read image in grayscale.\n\n"
     << "--brightness [-modVal=value] - modify brightness of an image.\n"
-    << "\t -modVal - integral value to add to each pixel (can be negative).\n"
-    << "--contrast [-modVal=value] - modify contrast of an image.\n"
-    << "\t -modVal - floating-point value of gamma variable.\n";
+    << "\t -modVal - integral value to add to each pixel (can be negative).\n\n"
+    << "--contrastLinear[-modVal=value] - modify contrast of an image using linear contrast stretching.\n"
+    << "\t -modVal - new value added and subtracted from upper and lower boundary.\n\n"
+    << "--contrastGamma [-modVal=value] - modify contrast of an image using gamma correction.\n"
+    << "\t -modVal - floating-point value of gamma variable.\n\n"
+    << "--negative - take negative of an image.\n\n";
 }
 
 // Command-line input variables
@@ -29,8 +33,11 @@ string outputFileName = "output.bmp";
 ImreadModes imreadMode = IMREAD_COLOR;
 bool isBrightnessModified = false;
 int brightnessModVal;
-bool isContrastModified = false;
-float contrastModVal;
+bool isContrastLinearModified = false;
+int contrastLinearModVal;
+bool isContrastGammaModified = false;
+float contrastGammaModVal;
+bool isNegative = false;
 
 // Process user input from command-line
 void processInput(int argc, char** argv) {
@@ -69,24 +76,46 @@ void processInput(int argc, char** argv) {
                 isBrightnessModified = false;
             }
         }
-        else if (static_cast<string>(argv[i]) == "--contrast") {
-            isContrastModified = true;
+        else if (static_cast<string>(argv[i]) == "--contrastLinear") {
+            isContrastLinearModified = true;
             string paramVal = argv[++i];
             if (paramVal.substr(0, 8) != "-modVal=") {
-                cout << "The --contrast parameter has to be of format: -modVal=[value]. Skipping contrast modification.";
-                isContrastModified = false;
+                cout << "The --contrastLinear parameter has to be of format: -modVal=[value]. Skipping linear contrast modification.";
+                isContrastLinearModified = false;
             }
             try {
-                contrastModVal = stof(paramVal.substr(paramVal.find('=') + 1));
+                contrastLinearModVal = stoi(paramVal.substr(paramVal.find('=') + 1));
             }
             catch (const invalid_argument & e) {
-                cout << "-modVal has to be an integer. Skipping contrast modification.";
-                isContrastModified = false;
+                cout << "-modVal has to be an integer. Skipping linear contrast modification.";
+                isContrastLinearModified = false;
             }
             catch (const out_of_range & e) {
-                cout << "-modVal is out of bounds. Skipping contrast modification.";
-                isContrastModified = false;
+                cout << "-modVal is out of bounds. Skipping linear contrast modification.";
+                isContrastLinearModified = false;
             }
+        }
+        else if (static_cast<string>(argv[i]) == "--contrastGamma") {
+            isContrastGammaModified = true;
+            string paramVal = argv[++i];
+            if (paramVal.substr(0, 8) != "-modVal=") {
+                cout << "The --contrastGamma parameter has to be of format: -modVal=[value]. Skipping gamma contrast modification.";
+                isContrastGammaModified = false;
+            }
+            try {
+                contrastGammaModVal = stof(paramVal.substr(paramVal.find('=') + 1));
+            }
+            catch (const invalid_argument & e) {
+                cout << "-modVal has to be an integer. Skipping gamma contrast modification.";
+                isContrastGammaModified = false;
+            }
+            catch (const out_of_range & e) {
+                cout << "-modVal is out of bounds. Skipping gamma contrast modification.";
+                isContrastGammaModified = false;
+            }
+        }
+        else if (static_cast<string>(argv[i]) == "--negative") {
+            isNegative = true;
         }
     }
 }
@@ -100,6 +129,10 @@ void saveImage(Mat image) {
 
 int main(int argc, char** argv) {
     processInput(argc, argv);
+    if (static_cast<string>(argv[1]) == "--help") {
+        return 0;
+    }
+
     Mat image = imread(argv[INPUT_IMAGE_POS], imreadMode);
 
     if (!image.data) {
@@ -116,16 +149,24 @@ int main(int argc, char** argv) {
         imageProcessor = make_unique<GrayscaleImageProcessor>(
             image,
             brightnessModVal,
-            contrastModVal
+            contrastLinearModVal,
+            contrastGammaModVal
             );
     }
 
     if (isBrightnessModified) {
         imageProcessor->modifyBrightness();
     }
-    if (isContrastModified) {
-        imageProcessor->modifyContrast();
+    if (isContrastLinearModified) {
+        imageProcessor->mofifyContrastLinear();
     }
+    if (isContrastGammaModified) {
+        imageProcessor->modifyContrastGamma();
+    }
+    if (isNegative) {
+        imageProcessor->negative();
+    }
+
     saveImage(image);
     return 0;
 }
