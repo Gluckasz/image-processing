6c621e065e06704ccfe7121bb3cb2465cb8831d9
Gluckasz <lukas.gluck.04@gmail.com>
2024-10-11 19:43:14 +0200

    Added TODO to main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "../include/InputProcessor.h"
-// TODO implement copyto method.
+// TODO implement copyto method, optimize space complexity.
 int main(int argc, char** argv) {
     InputProcessor inputProcessor(argc, argv);;
     inputProcessor.processInput();
