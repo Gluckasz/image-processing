ce43b9b51b316b746d018bf1b97f4c16cbf4f5c9
Gluckasz <lukas.gluck.04@gmail.com>
2025-02-02 13:34:20 +0100

    Moved HistogramProcessor to the namespace

--- include/HistogramProcessor.h
+++ include/HistogramProcessor.h
@@ -8,14 +8,13 @@
 #include <opencv2/opencv.hpp>
 
 
-class HistogramProcessor {
-public:
+namespace HistogramProcessor {
     /**
      * Compute histogram of the image for a specified channel.
      * @param image Input image
      * @return Array containing histogram values
      */
-    [[nodiscard]] static std::array<uint, UCHAR_MAX + 1> computeHistogram(cv::Mat image);
+    [[nodiscard]] std::array<uint, UCHAR_MAX + 1> computeHistogram(cv::Mat image);
 
     /**
      * Generate histogram visualization.
@@ -23,74 +22,75 @@ public:
      * @param binWidth Width of every bin in a histogram (in pixels)
      * @return Image containing histogram visualization
      */
-    static cv::Mat histogramVisualization(std::array<uint, UCHAR_MAX + 1> channelHistogram, int binWidth);
+    cv::Mat histogramVisualization(std::array<uint, UCHAR_MAX + 1> channelHistogram, int binWidth);
 
     /**
      * Perform histogram equalization.
      * @param image Image to equalize
+     * @param imageHistogram Input a histogram array
      * @param gMax Maximum output intensity
      * @param gMin Minimum output intensity
      * @return Transformed image with equalized histogram
      */
-    static cv::Mat histogramUniform(const cv::Mat& image, const std::array<uint, UCHAR_MAX + 1> &imageHistogram, int gMax, int gMin);
+    cv::Mat histogramUniform(const cv::Mat &image, const std::array<uint, UCHAR_MAX + 1> &imageHistogram, int gMax,
+                             int gMin);
 
     /**
      * Calculate mean value from histogram.
      * @param imageHistogram Input a histogram array
      * @return Mean value
      */
-    static double mean(const std::array<uint, UCHAR_MAX + 1> &imageHistogram);
+    double mean(const std::array<uint, UCHAR_MAX + 1> &imageHistogram);
 
     /**
      * Calculate variance from histogram.
      * @param imageHistogram Input a histogram array
      * @return Variance value
      */
-    static double variance(std::array<uint, UCHAR_MAX + 1> imageHistogram);
+    double variance(std::array<uint, UCHAR_MAX + 1> imageHistogram);
 
     /**
      * Calculate standard deviation from histogram.
      * @param imageHistogram Input a histogram array
      * @return Standard deviation value
      */
-    static double standardDeviation(const std::array<uint, UCHAR_MAX + 1> &imageHistogram);
+    double standardDeviation(const std::array<uint, UCHAR_MAX + 1> &imageHistogram);
 
     /**
      * Calculate the first variation coefficient from histogram.
      * @param imageHistogram Input a histogram array
      * @return First variation coefficient
      */
-    static double variation1(std::array<uint, UCHAR_MAX + 1> imageHistogram);
+    double variation1(std::array<uint, UCHAR_MAX + 1> imageHistogram);
 
     /**
      * Calculate asymmetry coefficient from histogram.
      * @param imageHistogram Input a histogram array
      * @return Asymmetry coefficient
      */
-    static double asymmetry(std::array<uint, UCHAR_MAX + 1> imageHistogram);
+    double asymmetry(std::array<uint, UCHAR_MAX + 1> imageHistogram);
 
     /**
      * Calculate flattening coefficient from histogram.
      * @param imageHistogram Input a histogram array
      * @return Flattening coefficient
      */
-    static double flattening(std::array<uint, UCHAR_MAX + 1> imageHistogram);
+    double flattening(std::array<uint, UCHAR_MAX + 1> imageHistogram);
 
     /**
      * Calculate the second variation coefficient from histogram.
      * @param imageHistogram Input a histogram array
      * @return Second variation coefficient
      */
-    static double variation2(const std::array<uint, UCHAR_MAX + 1> &imageHistogram);
+    double variation2(const std::array<uint, UCHAR_MAX + 1> &imageHistogram);
 
     /**
      * Calculate entropy from histogram.
      * @param imageHistogram Input a histogram array
      * @return Entropy value
      */
-    static double entropy(const std::array<uint, UCHAR_MAX + 1> &imageHistogram);
-};
-
+    double entropy(const std::array<uint, UCHAR_MAX + 1> &imageHistogram);
+}
 
 
 #endif //HISTOGRAMPROCESSOR_H
--- src/HistogramProcessor.cpp
+++ src/HistogramProcessor.cpp
@@ -4,143 +4,145 @@
 
 #include "../include/HistogramProcessor.h"
 
-std::array<uint, UCHAR_MAX + 1> HistogramProcessor::computeHistogram(cv::Mat image) {
-    std::array<uint, UCHAR_MAX + 1> intensityCountArray = {};
+namespace HistogramProcessor {
+    std::array<uint, UCHAR_MAX + 1> computeHistogram(cv::Mat image) {
+        std::array<uint, UCHAR_MAX + 1> intensityCountArray = {};
 
-    for (int x = 0; x < image.rows; x++) {
-        for (int y = 0; y < image.cols; y++) {
-            const uint intensity = image.at<uchar>(x, y);
-            intensityCountArray[intensity]++;
+        for (int x = 0; x < image.rows; x++) {
+            for (int y = 0; y < image.cols; y++) {
+                const uint intensity = image.at<uchar>(x, y);
+                intensityCountArray[intensity]++;
+            }
         }
+
+        return intensityCountArray;
     }
 
-    return intensityCountArray;
-}
+    cv::Mat histogramVisualization(std::array<uint, UCHAR_MAX + 1> channelHistogram,
+                                   const int binWidth) {
+        const int histogramHeight = static_cast<int>(*std::ranges::max_element(channelHistogram));
 
-cv::Mat HistogramProcessor::histogramVisualization(std::array<uint, UCHAR_MAX + 1> channelHistogram,
-                                                   const int binWidth) {
-    const int histogramHeight = static_cast<int>(*std::ranges::max_element(channelHistogram));
-
-    const int histogramWidth = (UCHAR_MAX + 1) * binWidth;
-    cv::Mat histogramImage = cv::Mat::zeros(histogramHeight, histogramWidth + binWidth, CV_8UC1);
-    for (int y = 0; y < histogramWidth; y++) {
-        for (int x = 0; x < channelHistogram[y / binWidth]; x++) {
-            histogramImage.at<uchar>(histogramHeight - x - 1, y) = UCHAR_MAX;
+        const int histogramWidth = (UCHAR_MAX + 1) * binWidth;
+        cv::Mat histogramImage = cv::Mat::zeros(histogramHeight, histogramWidth + binWidth, CV_8UC1);
+        for (int y = 0; y < histogramWidth; y++) {
+            for (int x = 0; x < channelHistogram[y / binWidth]; x++) {
+                histogramImage.at<uchar>(histogramHeight - x - 1, y) = UCHAR_MAX;
+            }
         }
-    }
-    return histogramImage;
-}
-
-cv::Mat HistogramProcessor::histogramUniform(const cv::Mat &image,
-                                             const std::array<uint, UCHAR_MAX + 1> &imageHistogram, const int gMax,
-                                             const int gMin) {
-    cv::Mat result = image.clone();
-    const int totalPixels = image.rows * image.cols;
-    uint cdf[256] = {static_cast<uint>(imageHistogram[0])};
-    for (int i = 1; i < 256; i++)
-        cdf[i] = cdf[i - 1] + imageHistogram[i];
-
-    uchar lut[256];
-    for (int i = 0; i < 256; i++) {
-        lut[i] = std::clamp(
-            gMin + static_cast<int>(std::round((gMax - gMin) * (static_cast<double>(cdf[i]) / totalPixels))),
-            0,
-            UCHAR_MAX
-        );
+        return histogramImage;
     }
 
-    result.forEach<uchar>([&](uchar &pixel, const int *) {
-        pixel = lut[pixel];
-    });
-    return result;
-}
+    cv::Mat histogramUniform(const cv::Mat &image,
+                             const std::array<uint, UCHAR_MAX + 1> &imageHistogram, const int gMax,
+                             const int gMin) {
+        cv::Mat result = image.clone();
+        const int totalPixels = image.rows * image.cols;
+        uint cdf[256] = {static_cast<uint>(imageHistogram[0])};
+        for (int i = 1; i < 256; i++)
+            cdf[i] = cdf[i - 1] + imageHistogram[i];
 
-
-double HistogramProcessor::mean(const std::array<uint, UCHAR_MAX + 1> &imageHistogram) {
-    double sum = 0;
-    double pixels = 0;
-    for (int i = 0; i < imageHistogram.size(); i++) {
-        sum += imageHistogram[i] * i;
-        pixels += imageHistogram[i];
-    }
-
-    return sum / pixels;
-}
-
-double HistogramProcessor::variance(std::array<uint, UCHAR_MAX + 1> imageHistogram) {
-    double sum = 0;
-    double pixels = 0;
-    double mean = mean(imageHistogram);
-    for (int i = 0; i < imageHistogram.size(); i++) {
-        sum += imageHistogram[i] * pow(i - mean, 2);
-        pixels += imageHistogram[i];
-    }
-
-    return sum / pixels;
-}
-
-double HistogramProcessor::standardDeviation(const std::array<uint, UCHAR_MAX + 1> &imageHistogram) {
-    const double variance = HistogramProcessor::variance(imageHistogram);
-    return sqrt(variance);
-}
-
-double HistogramProcessor::variation1(std::array<uint, UCHAR_MAX + 1> imageHistogram) {
-    double variance = variance(imageHistogram);
-    double mean = mean(imageHistogram);
-    return variance / mean;
-}
-
-double HistogramProcessor::asymmetry(std::array<uint, UCHAR_MAX + 1> imageHistogram) {
-    double sum = 0;
-    double pixels = 0;
-    double mean = mean(imageHistogram);
-    for (int i = 0; i < imageHistogram.size(); i++) {
-        sum += imageHistogram[i] * pow(i - mean, 3);
-        pixels += imageHistogram[i];
-    }
-
-    double standardDeviation = standardDeviation(imageHistogram);
-
-    return sum / pixels / pow(standardDeviation, 3);
-}
-
-double HistogramProcessor::flattening(std::array<uint, UCHAR_MAX + 1> imageHistogram) {
-    double sum = 0;
-    double pixels = 0;
-    double mean = mean(imageHistogram);
-    for (int i = 0; i < imageHistogram.size(); i++) {
-        sum += imageHistogram[i] * pow(i - mean, 4);
-        pixels += imageHistogram[i];
-    }
-
-    double variance = variance(imageHistogram);
-
-    return sum / pixels / pow(variance, 2) - 3;
-}
-
-double HistogramProcessor::variation2(const std::array<uint, UCHAR_MAX + 1> &imageHistogram) {
-    double sum = 0;
-    double pixels = 0;
-    for (const unsigned int i: imageHistogram) {
-        sum += pow(i, 2);
-        pixels += i;
-    }
-
-    return sum / pow(pixels, 2);
-}
-
-double HistogramProcessor::entropy(const std::array<uint, UCHAR_MAX + 1> &imageHistogram) {
-    double sum = 0;
-    double pixels = 0;
-    for (const unsigned int i: imageHistogram) {
-        pixels += i;
-    }
-
-    for (const unsigned int i: imageHistogram) {
-        if (i > 0) {
-            sum += i * log2(i / pixels);
+        uchar lut[256];
+        for (int i = 0; i < 256; i++) {
+            lut[i] = std::clamp(
+                gMin + static_cast<int>(std::round((gMax - gMin) * (static_cast<double>(cdf[i]) / totalPixels))),
+                0,
+                UCHAR_MAX
+            );
         }
+
+        result.forEach<uchar>([&](uchar &pixel, const int *) {
+            pixel = lut[pixel];
+        });
+        return result;
     }
 
-    return -sum / pixels;
+
+    double mean(const std::array<uint, UCHAR_MAX + 1> &imageHistogram) {
+        double sum = 0;
+        double pixels = 0;
+        for (int i = 0; i < imageHistogram.size(); i++) {
+            sum += imageHistogram[i] * i;
+            pixels += imageHistogram[i];
+        }
+
+        return sum / pixels;
+    }
+
+    double variance(std::array<uint, UCHAR_MAX + 1> imageHistogram) {
+        double sum = 0;
+        double pixels = 0;
+        double mean = mean(imageHistogram);
+        for (int i = 0; i < imageHistogram.size(); i++) {
+            sum += imageHistogram[i] * pow(i - mean, 2);
+            pixels += imageHistogram[i];
+        }
+
+        return sum / pixels;
+    }
+
+    double standardDeviation(const std::array<uint, UCHAR_MAX + 1> &imageHistogram) {
+        const double variance = HistogramProcessor::variance(imageHistogram);
+        return sqrt(variance);
+    }
+
+    double variation1(std::array<uint, UCHAR_MAX + 1> imageHistogram) {
+        double variance = variance(imageHistogram);
+        double mean = mean(imageHistogram);
+        return variance / mean;
+    }
+
+    double asymmetry(std::array<uint, UCHAR_MAX + 1> imageHistogram) {
+        double sum = 0;
+        double pixels = 0;
+        double mean = mean(imageHistogram);
+        for (int i = 0; i < imageHistogram.size(); i++) {
+            sum += imageHistogram[i] * pow(i - mean, 3);
+            pixels += imageHistogram[i];
+        }
+
+        double standardDeviation = standardDeviation(imageHistogram);
+
+        return sum / pixels / pow(standardDeviation, 3);
+    }
+
+    double flattening(std::array<uint, UCHAR_MAX + 1> imageHistogram) {
+        double sum = 0;
+        double pixels = 0;
+        double mean = mean(imageHistogram);
+        for (int i = 0; i < imageHistogram.size(); i++) {
+            sum += imageHistogram[i] * pow(i - mean, 4);
+            pixels += imageHistogram[i];
+        }
+
+        double variance = variance(imageHistogram);
+
+        return sum / pixels / pow(variance, 2) - 3;
+    }
+
+    double variation2(const std::array<uint, UCHAR_MAX + 1> &imageHistogram) {
+        double sum = 0;
+        double pixels = 0;
+        for (const unsigned int i: imageHistogram) {
+            sum += pow(i, 2);
+            pixels += i;
+        }
+
+        return sum / pow(pixels, 2);
+    }
+
+    double entropy(const std::array<uint, UCHAR_MAX + 1> &imageHistogram) {
+        double sum = 0;
+        double pixels = 0;
+        for (const unsigned int i: imageHistogram) {
+            pixels += i;
+        }
+
+        for (const unsigned int i: imageHistogram) {
+            if (i > 0) {
+                sum += i * log2(i / pixels);
+            }
+        }
+
+        return -sum / pixels;
+    }
 }
