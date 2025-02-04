caa5ff82039035303550e41941e1678c331ff145
Gluckasz <lukas.gluck.04@gmail.com>
2025-02-04 14:22:32 +0100

    Reformatted CompareImageStatsOperation.cpp

--- src/stats-operations/CompareImageStatsOperation.cpp
+++ src/stats-operations/CompareImageStatsOperation.cpp
@@ -5,7 +5,6 @@
 
 #include "ImageComparer.h"
 #include "ImageOperation.h"
-#include "MorphologicalProcessor.h"
 #include "OutputManager.h"
 
 class CompareImageStatsOperation final : public ImageOperation {
@@ -19,9 +18,9 @@ public:
 
     void apply(cv::Mat &newImage) const override {
         std::vector<cv::Mat> noiseImageChannels, noNoiseImageChannels, newImageChannels;
-        cv::split(noiseImage_, noiseImageChannels);
-        cv::split(noNoiseImage_, noNoiseImageChannels);
-        cv::split(newImage, newImageChannels);
+        split(noiseImage_, noiseImageChannels);
+        split(noNoiseImage_, noNoiseImageChannels);
+        split(newImage, newImageChannels);
         double mseBefore = 0, mseAfter = 0, pmseBefore = 0, pmseAfter = 0, snrBefore = 0, snrAfter = 0, psnrBefore = 0,
                 psnrAfter = 0, mdBefore = 0, mdAfter = 0;
         for (int i = 0; i < newImageChannels.size(); i++) {
@@ -39,16 +38,25 @@ public:
         std::stringstream ss;
         ss << "Comparison image stats:\n";
         ss << "Mean square error before denoising: " << mseBefore / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Mean square error after denoising: " << mseAfter / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Peak mean square error before denoising: " << pmseBefore / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Peak mean square error after denoising: " << pmseAfter / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Signal to noise ratio before denoising: " << snrBefore / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Signal to noise ratio after denoising: " << snrAfter / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Peak signal to noise ratio before denoising: " << psnrBefore / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Peak signal to noise ratio before denoising: " << psnrAfter / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Maximum difference before denoising: " << mdBefore / static_cast<double>(newImageChannels.size()) << "\n"
-        << "Maximum difference after denoising: " << mdAfter / static_cast<double>(newImageChannels.size()) << "\n";
-        std::string path = OutputManager::constructPath("image", "comparison_stats", "txt");
+                << "Mean square error after denoising: " << mseAfter / static_cast<double>(newImageChannels.size()) <<
+                "\n"
+                << "Peak mean square error before denoising: " << pmseBefore / static_cast<double>(newImageChannels.
+                    size()) << "\n"
+                << "Peak mean square error after denoising: " << pmseAfter / static_cast<double>(newImageChannels.
+                    size()) << "\n"
+                << "Signal to noise ratio before denoising: " << snrBefore / static_cast<double>(newImageChannels.
+                    size()) << "\n"
+                << "Signal to noise ratio after denoising: " << snrAfter / static_cast<double>(newImageChannels.size())
+                << "\n"
+                << "Peak signal to noise ratio before denoising: " << psnrBefore / static_cast<double>(newImageChannels.
+                    size()) << "\n"
+                << "Peak signal to noise ratio before denoising: " << psnrAfter / static_cast<double>(newImageChannels.
+                    size()) << "\n"
+                << "Maximum difference before denoising: " << mdBefore / static_cast<double>(newImageChannels.size()) <<
+                "\n"
+                << "Maximum difference after denoising: " << mdAfter / static_cast<double>(newImageChannels.size()) <<
+                "\n";
+        const std::string path = OutputManager::constructPath("image", "comparison_stats", "txt");
         OutputManager::saveFile(path, ss);
     }
 };
